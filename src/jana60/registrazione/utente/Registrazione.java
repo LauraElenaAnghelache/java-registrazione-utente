d758f056ac8b935ac4b63a52ab2583467b9c5240
LauraElenaAnghelache <laura.elena.anghelache@outlook.it>
2022-06-24 15:53:46 +0200

    first commit

---
+++ src/jana60/registrazione/utente/Registrazione.java
@@ -0,0 +1,10 @@
+package jana60.registrazione.utente;
+
+public class Registrazione {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/jana60/registrazione/utente/Utente.java
@@ -0,0 +1,10 @@
+package jana60.registrazione.utente;
+
+public class Utente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
