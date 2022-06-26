5416ecea274c52c36fbf1742d7f8d87c474b9ac3
LauraElenaAnghelache <laura.elena.anghelache@outlook.it>
2022-06-27 01:27:59 +0200

    last commit

--- src/jana60/registrazione/utente/Registrazione.java
+++ src/jana60/registrazione/utente/Registrazione.java
@@ -1,9 +1,44 @@
 package jana60.registrazione.utente;
 
+import java.util.Scanner;
+
 public class Registrazione {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		try {
+			System.out.println("inserire il nome: ");
+			String nome = scan.nextLine();
+			System.out.println("inserire il cognome: ");
+			String cognome = scan.nextLine();
+			
+			System.out.println("inserire l'età: ");
+			int eta = Integer.parseInt(scan.nextLine());
+			
+			System.out.println("Inserire la  password: ");
+			String password = scan.nextLine();
+			System.out.println("inserire email: ");
+			String email = scan.nextLine();
+			
+			Utente nuovoUtente = new Utente(nome, cognome, eta , email, password);
+			
+			System.out.println("Account creato con i dati: ");
+			System.out.println(nuovoUtente.toString());
+			
+		} catch(NumberFormatException nfe) {
+			System.out.println("l'input inserito non è un numero valido");
+			System.out.println(nfe.getMessage());
+		} catch(Exception e) {
+			System.out.println("impossibile creare l'account");
+			System.out.println(e.getMessage()); 
+		} finally {
+			System.out.println("Arrivederci!");
+		}
+		
+		
+		scan.close();
+
 
 	}
 
--- src/jana60/registrazione/utente/Utente.java
+++ src/jana60/registrazione/utente/Utente.java
@@ -2,9 +2,86 @@ package jana60.registrazione.utente;
 
 public class Utente {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	/*Creare una classe Utente con attributi nome, cognome, email, password ed età.
+Aggiungere un costruttore con parametri e metodi getter e setter.
+Implementare dei metodi di validazione in modo che:
+- email contenga un carattere @ e un .
+- password sia lunga tra 8 e 12 caratteri
+- età sia maggiore di 18 anni
+In caso i parametri non siano validi, impedire che vengano valorizzati gli attributi nei metodi e nel costruttore, sollevando opportune eccezioni.
+Creare una classe Registrazione con metodo main in cui, tramite Scanner, vengono chieste le informazioni all’utente e si prova a creare un nuovo oggetto Utente.
+Gestire tramite try e catch eventuali errori in fase di creazione in modo che il programma non si interrompa bruscamente ma chieda all’utente di inserire dei dati corretti.
+*/
+	//ATTRIBUTI
+	private String nome, cognome, email, password;
+	private int eta;
+	
+	//COTRUTTORE
+	public Utente(String nome, String cognome, int eta , String email, String password) throws Exception {
+		super();
+		 this.nome = nome;
+			this.cognome = cognome;
+			this.email = email;
+			this.password = password;
+			this.eta = eta;
+		
+	    if(!(email.contains("@")))
+		    throw new Exception("L'indirizzo deve contenere il carattere @");
+	    if(password.length() < 8)
+		    throw new Exception("La password deve contenere un minimo di 8 caratteri!");
+			 if (password.length() > 12)
+		    throw new Exception("La password deve contenere un massimo di 12 caratteri!");		
+			 if(eta <18)
+					throw new Exception("Maggiore età richiesta!");
+           
+	}		
+		
+	//GETTER E SETTER
+	public String getNome() {
+		return nome;
 	}
 
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getEta() {
+		return eta;
+	}
+
+	public void setEta(int eta) {
+		this.eta = eta;
+	}
+	  
+public String toString( ) {
+	return "Nome:" + nome + "\n" + "Cognome:" + cognome + "\n" + "Eta:" + eta + "\n" + "Email:" + email + "\n" + password;
 }
+}
+	
+	
+
+
